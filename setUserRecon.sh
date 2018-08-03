dc828e0ad58e2f5d63d93abe7d1e4f486d0bd324
Zach Dorow <zdorow@me.com>
2018-08-03 17:27:28 -0500

    userRecon

---
+++ setUserRecon.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+loggedInUser=$( python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");' )
+
+jamf recon -endUsername $loggedInUser
+
+jamf recon
+
